4389090f71a04b52d77c6adba0f2bcc907609635
BarujHaro <fernando.haro.sal@gmail.com>
2025-04-15 22:41:37 -0600

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./scr/index.js",
+  mode: "production",
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/",
   },
   plugins: [
     new HtmlWebpackPlugin({
