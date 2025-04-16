a78e422040b598e93bf2df77f3cf0a966cdeb8e2
BarujHaro <fernando.haro.sal@gmail.com>
2025-04-15 23:22:14 -0600

    update 2

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: "/",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -34,7 +33,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][ext][query]", // Guarda en `dist/images/`
+          filename: "images/[name][ext][query]", 
         },
       },
     ],
